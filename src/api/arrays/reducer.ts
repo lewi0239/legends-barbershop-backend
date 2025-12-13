86c5df3a4bef3a114bf52bdf7391336ea88262c5
Brodie Lewis <lewis.evault@gmail.com>
2025-12-13 00:05:11 -0500

    dev: Completed arrays/reducer.ts method

---
+++ src/api/arrays/reducer.ts
@@ -0,0 +1,73 @@
+declare global {
+  interface Array<T> {
+    myReduce<U>(
+      callback: (
+        accumulator: U,
+        currentValue: T,
+        index: number,
+        array: T[]
+      ) => U,
+      initialValue: U
+    ): U;
+    myReduce(
+      callback: (
+        accumulator: T,
+        currentValue: T,
+        index: number,
+        array: T[]
+      ) => T
+    ): T;
+  }
+}
+
+Array.prototype.myReduce = function <T, U>(
+  this: T[],
+  callback: (
+    accumulator: U | T,
+    currentValue: T,
+    index: number,
+    array: T[]
+  ) => U | T,
+  initialValue?: U
+): U | T {
+  if (typeof callback !== "function") {
+    throw new TypeError(callback + " is not a function");
+  }
+
+  const arr = this;
+
+  const len = arr.length;
+
+  const hasInitialValue = arguments.length > 1;
+  let accumulator: any = hasInitialValue ? initialValue : undefined;
+  let startIndex = 0;
+
+  if (!hasInitialValue) {
+    while (startIndex < len && !(startIndex in arr)) {
+      startIndex++;
+    }
+
+    if (startIndex >= len) {
+      throw new TypeError("Reduce of empty array with no initial value");
+    }
+
+    accumulator = arr[startIndex++];
+  }
+
+  for (let i = startIndex; i < len; i++) {
+    if (i in arr) {
+      accumulator = callback(accumulator, arr[i], i, arr);
+    }
+  }
+
+  return accumulator;
+};
+
+export {};
+
+//Tests:
+// console.log([1, 2, 3].myReduce((a, b) => a + b, 0)); // ✅ 6
+// console.log([1, 2, 3].myReduce((a, b) => a + b, 4)); // ✅ 10
+// console.log([1].myReduce((a, b) => a + b)); // ✅ 1
+// console.log([].myReduce((a, b) => a + b, 5)); // ✅ 5
+// console.log([].myReduce((a, b) => a + b)); // ❌ throws (correct!)
